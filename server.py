28a7311f795ab378f42ef9e4e43f99d76f856232
Isaiah Nagelkerk <isaiah.nagelkerk@gmail.com>
2023-04-04 01:24:41 +0000

    added random, string

--- server.py
+++ server.py
@@ -15,6 +15,8 @@ from sqlalchemy import *
 from sqlalchemy.pool import NullPool
 from flask import Flask, request, render_template, g, redirect, Response
 import psycopg2
+import random
+import string
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
