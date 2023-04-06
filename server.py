3deff6fe8e2d2aabd69ea1a188a53effd74fd97c
Isaiah Nagelkerk <isaiah.nagelkerk@gmail.com>
2023-04-06 17:30:10 +0000

    reformatted insert statment added ?

--- server.py
+++ server.py
@@ -224,14 +224,14 @@ def log_training_event():
         py_event_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
         #training_event = [(training_type1, date1, start1, finish1, miles1, event_id)]
         # Insert the new training event into the training_event table
-        insert_command = """INSERT INTO training_event(training_type, date, start, finish, miles, event_id) VALUES (py_training_type, py_date, py_start, py_finish, py_miles, py_event_id)"""
+        insert_command = """INSERT INTO training_event(training_type, date, start, finish, miles, event_id) VALUES (?, ?, ?, ?, ?, ?), (py_training_type, py_date, py_start, py_finish, py_miles, py_event_id)"""
         res = g.conn.execute(text(insert_command))
         #cursor.execute("INSERT INTO training_event (training_type, date, start, finish, miles, event_id) VALUES (%s, %s, %s, %s, %s, %s)", (training_type, date, start, finish, miles, event_id))
         #g.conn.commit()
         g.conn.commit()
         res.close()
         # Insert the new log entry into the log table
-        log_insert_command = """INSERT INTO log(runner_id, event_id) VALUES (py_runner_id, py_event_id)"
+        log_insert_command = """INSERT INTO log(runner_id, event_id) VALUES (?, ?), (py_runner_id, py_event_id)"
         res2 = g.conn.execute(text(log_insert_command))"""
         g.conn.commit()
         #cursor.execute("INSERT INTO log (runner_id, event_id) VALUES (%s, %s)", (runner_id, event_id))
